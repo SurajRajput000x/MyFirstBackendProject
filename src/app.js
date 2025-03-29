0c34ed87e7c57e78125afc8180777f745ab75fee
Suraj Rajput <surajrajput000x.com>
2025-03-29 22:04:54 +0530

    all setup configured

--- src/app.js
+++ src/app.js
@@ -21,6 +21,13 @@ app.use(express.static("public"))
 
 
 
+// routes import
+import userRouter from './routes/user.routes.js'
+
+
+
+// routes declaration
+app.use("/api/v1/users", userRouter)
 
 
 
--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 // require('dotenv').config({path:'./env'})
 import dotenv from 'dotenv'
 import connectDB from './db/db.js'
+
+import {app} from './app.js'
 dotenv.config({path:'./.env'})
 
 
